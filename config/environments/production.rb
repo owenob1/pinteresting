7fe36ce9d4e7915d10bb04abcacfcf663f174354
Owen Innes <owenob1@gmail.com>
2013-11-01 14:06:19 +1100

    Add Amazon S3 for Paperclip uploads to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Pinteresting::Application.configure do
 
   #Need to change for production
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  #Sets Paperclip to upload to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
