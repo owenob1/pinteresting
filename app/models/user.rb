6254edcb03b55e7b69f40250c5be75e06078d0af
Owen Innes <owenob1@gmail.com>
2013-11-02 14:13:31 +1100

    Added Username Sign Up and Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    class User < ActiveRecord::Base
 
-   has_many :pins
-end
+      attr_accessor :login
+      devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+
+      validates_uniqueness_of :username
+      validates_presence_of :username
+      validates :username, length: { in: 4..20 }
+
+      def self.find_first_by_auth_conditions(warden_conditions)
+        conditions = warden_conditions.dup
+        if login = conditions.delete(:login)
+          where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+          else
+            where(conditions).first
+          end
+      end 
+
+      has_many :pins     
+    end
